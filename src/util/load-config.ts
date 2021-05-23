5301f45051ca8d6bc461d9530c3f3fdf77ccfab2
Jon Webb <Jon@jonwebb.dev>
2021-05-23 17:46:13 -0400

    use handler function

--- src/util/load-config.ts
+++ src/util/load-config.ts
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
 import type { Schema } from "joi";
 
-import { logger } from "./logger";
+import { handle } from "./error";
 
 export const loadConfig = (schema: Schema) => {
   dotenv.config();
 
   const { value, error } = schema.validate(process.env);
   if (error) {
-    logger.fatal(new Error(`Invalid environment: ${error.message}`));
+    handle(new Error(`Invalid environment: ${error.message}`));
   }
   return value;
 };
